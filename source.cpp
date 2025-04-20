811dc59ec1a208ac0ccd507557255bd39c4433b3
Rana Mitchell <2403597@uad.ac.uk>
2025-04-20 21:40:49 +0100

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -20,30 +20,92 @@ XOR -> true if only one input is true, else false.
 -------------------------
 */
 
-int main() // https://cplusplus.com/forum/general/224178/
+string XORencrypt(string userInput, string XORinput)
+{
+	string binaryUser = "";
+	string binaryXOR = "";
+	string XORencrypt = "";
+	char add;
+
+	for (size_t i = 0; i < userInput.length(); i++)
+	{
+		binaryUser = ASCIItoBinary(userInput[i]);
+		binaryXOR = ASCIItoBinary(XORinput[i]);
+
+		add = binaryUser ^ binaryXOR;
+		XORencrypt = XORencrypt + add;
+	}
+
+	return XORencrypt;
+}
+
+string XORdecrypt(string binaryUser, string binaryXOR)
+{
+	string XORdecrypt = "";
+	return XORdecrypt;
+}
+
+string ASCIItoBinary(char inputChar)
+{
+	string binaryString;
+	binaryString = bitset<8>(inputChar).to_string(); // converting the output to a string to make it storable
+	return binaryString;
+}
+
+int main()
 {
 	string userInput = "";
-	string xorInput = "";
+	string XORinput = "";
+
+	string userSentence[50] = {};
+	string XORsentence[50] = {};
+
+	int inputSize = 0;
 	int check = 0;
 
+	cout << "Welcome!\n";
+
 	do {
-			cout << "Enter a sentence: ";
+			cout << "Enter a sentence, without spaces: ";
 			cin >> userInput;
+			inputSize = userInput.length();
 
-			cout << "Enter a sentence to XOR with (has to be the same length): ";
-			cin >> xorInput;
+			cout << "Enter a sentence " << inputSize << " characters long: ";
+			cin >> XORinput;
+			cout << endl;
 
-		if (userInput.length() != xorInput.length())
+		if (userInput.length() != XORinput.length())
 		{
 			cout << "Please enter sentences that are the same length!" << endl;
 			check = 0;
 		}
-		else if (userInput.length() == xorInput.length())
+		else if (userInput.length() == XORinput.length())
 		{
 			check = 1;
 		}
 
 	} while (check != 1);
+	
+	for (size_t i = 0; i < userInput.length(); i++) // "size_t is an unsigned integer data type...used to represent the size of objects in bytes..." (geeksforgeeks, 2025) https://www.geeksforgeeks.org/size_t-data-type-c-language/
+	{ // for the length of the inputted string...
+		// cout << "USER SENTENCE: ";
+		userSentence[i] = ASCIItoBinary(userInput[i]); // run the ASCIItoBinary function on the current userInput value
+		// cout << userSentence[i] << " " << endl;
+		
+	}
+
+	for (size_t i = 0; i < XORinput.length(); i++) 
+	{ 
+		// cout << "XOR SENTENCE: ";
+		XORsentence[i] = ASCIItoBinary(XORinput[i]);
+		// cout << XORsentence[i] << " " << endl;
+
+	}
+
+	// cout << "userSentence[4] = " << userSentence[4] << endl;
+
+	XORencrypt(userInput, XORinput);
+	
 
 return 0;
 }
